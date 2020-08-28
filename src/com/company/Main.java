09595c0f9807c2d82a3113760c58758c8211864b
John Balatsos <westcoast1910@hotmail.com>
2020-08-28 19:27:00 +0300

    - Add new method printSomething - Remove println

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello Git!");
-        System.out.println("New change");
+    }
+
+    public void printSomething() {
+        System.out.println("new method");
     }
 }
